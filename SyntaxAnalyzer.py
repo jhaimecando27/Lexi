84cc09ac251f16424cc8bdbb54f851ebe34b1bcf
J0nalD-T3nio <141342354+J0nalD-T3nio@users.noreply.github.com>
2024-01-31 00:42:05 +0800

    Add files via upload
    SyntaxAnalyzer.py => Para sa syntax analysis. 
                                  => Ang syntax_analysis function ay para sa syntax analyzer logic.
                                  => Ang display_tree function ay para gumawa ng bagong window a idisplay ang token stream at parse tree
                                  => Ang token_stream function ay para palitan ang codes/lexeme ng program sa token counterpart nila. Kailangan mauna tong matapos bago mapagana ang display_tree function.
    
    grammar.py => laman nito ang CFG, first set, follow set, at predict set na magdidikta ng syntax ng herbicode language.

---
+++ SyntaxAnalyzer.py
@@ -0,0 +1,15 @@
+# Syntax Analyzer Logic
+from grammar import cfg, first_set, follow_set, predict_set
+
+
+# For checking the syntax of the program
+def syntax_analysis():
+    pass
+
+# For displaying the Parse Tree
+def display_tree():
+    pass
+
+# For displaying the token stream e.g., tint a; == reserved word identifier reserved symbol
+def token_stream():
+    pass
