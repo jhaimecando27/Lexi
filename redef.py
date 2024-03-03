c52c83ca8674cfba929615bb24745eacc5504c7a
Jhaime Cando <74945153+jhaimecando27@users.noreply.github.com>
2024-03-03 15:53:40 +0800

    add delim24 - 28

--- redef.py
+++ redef.py
@@ -55,14 +55,17 @@ delim19 = ["1", "2", "3", "4", "5", "6", "7", "8", "9", ' ', "#"]
 
 # Regular Definitions:
 zero = "0"
-dig = "123456789"
-let = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+dig = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+let = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+       "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+       "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G",
+       "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+       "S", "T", "U", "V", "W", "X", "Y", "Z"]
 num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 newline = "\\n"
-diglet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-sep = ","
-ht = "#"
-space = "\" \""
+diglet = dig + let
+ht = ["#"]
+space = [" "]
 tab = "\\t"
 invalid = "`@$?"
 
@@ -70,6 +73,12 @@ delim20 = let
 delim21 = [":"]
 delim22 = num + [" ", "(", "#"]
 delim23 = [' ', '\n', '\"']
+delim24 = ["("]
+delim25 = space + [")", ","]
+delim26 = space + [")"]
+delim27 = space + num + ht + [",", "_", "["]
+delim28 = let
+
 
 # TOKENS
 
